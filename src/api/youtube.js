0cef55748acab9f9cd65d6fa75cffea42290bdc1
rhermadav <davisonuyigue@gmail.com>
2020-02-04 12:51:51 +0100

    respond to error

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyCOqMs8oSffAGq4xaJZsFdDeWPujH-hcYws';
+const KEY = '';
 
 export default axios.create({
    baseURL: 'https://www.googleapis.com/youtube/v3',
